df0e29052387fe2317484337809c2ee7189833ea
Matthew Goodman <mattgoodman3099@gmail.com>
2020-11-20 11:14:21 -0600

    phase2 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Heist
 {
@@ -7,15 +9,34 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
-            Console.WriteLine("Enter a team member's name.");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter a team member's skill level.");
-            string skillLevel = Console.ReadLine();
-            int skill = int.Parse(skillLevel);
-            Console.WriteLine("Enter a team member's courage.");
-            string courageLevel = Console.ReadLine();
-            double courage = double.Parse(courageLevel);
-            Console.WriteLine($@"Name: {name} Skill: {skill} Courge: {courage}");
+            List<TeamMember> listOfMembers = new List<TeamMember>();
+            while (true)
+            {
+                Console.WriteLine("Enter a team member's name.");
+                string name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a team member's skill level.");
+                    string skillLevel = Console.ReadLine();
+                    int skill = int.Parse(skillLevel);
+                    Console.WriteLine("Enter a team member's courage.");
+                    string courageLevel = Console.ReadLine();
+                    double courage = double.Parse(courageLevel);
+                    TeamMember member = new TeamMember(name, skill, courage);
+                    listOfMembers.Add(member);
+                }
+
+            }
+
+            Console.WriteLine($"Total number of Team Members: {listOfMembers.Count()}");
+            foreach (var member in listOfMembers)
+            {
+                Console.WriteLine($"Member {member.name}");
+            }
         }
     }
 }
